ecba6c9e511090bb8d05f3efd24be7baaa5d8a7c
Saher <saher.ziauddin@dal.ca>
2025-08-23 03:21:17 -0300

    Added necessary fields in order to create a booking

--- backend/booking/main.py
+++ backend/booking/main.py
@@ -16,15 +16,46 @@ db_config = {
 @app.route("/book", methods=["POST"])
 def book_agent():
     try:
-        # Get postal code from query parameters
-        postal_code = request.args.get('postal_code')
-        
+        data = request.get_json()
+
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor(dictionary=True)
         
-        return {"message": "received"}, 200
+        # Create entry into location table
+        cursor.execute("""
+            INSERT INTO locations (latitude, longitude, postal_code, street_name, street_number)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (data["location"]['latitude'], data["location"]['longitude'], data["location"]['postal_code'], data["location"]['street_name'], data["location"]['street_number']))
+        location_id = cursor.lastrowid
+
+        # Create entry into customers table
+        cursor.execute("""
+            INSERT INTO customers (name, email, phone, location_id)
+            VALUES (%s, %s, %s, %s)
+        """, (data["customer"]['name'], data["customer"]['email'], data["customer"]['phone'], location_id))
+        
+        customer_id = cursor.lastrowid
+
+        # Create new booking
+        cursor.execute("""
+            INSERT INTO bookings (agentId, customerId, booking_date)
+            VALUES (%s, %s, %s)
+        """, (data['agentId'], customer_id, data['booking_date']))
+        booking_id = cursor.lastrowid
+        
+        conn.commit()
+
+        return jsonify({
+            "message": "Booking created successfully",
+            "customerId": customer_id,
+            "bookingId": booking_id,
+            "bookingStatus": "confirmed"
+        }), 200
 
     except Exception as e:
+        if 'conn' in locals():
+            conn.rollback()
+     
         return jsonify({"error": str(e)}), 500
 
     finally:
