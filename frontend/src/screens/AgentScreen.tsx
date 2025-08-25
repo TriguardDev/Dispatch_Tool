6380f9e865799ef6511aa470eb5acb5c03e1e989
Saher <saher.ziauddin@dal.ca>
2025-08-24 23:42:08 -0300

    Added basic color screens

---
+++ frontend/src/screens/AgentScreen.tsx
@@ -0,0 +1,7 @@
+export default function AgentScreen() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-yellow-200">
+      <h1 className="text-2xl font-bold">Agent Screen</h1>
+    </div>
+  );
+}
---
+++ frontend/src/screens/CustomerScreen.tsx
@@ -0,0 +1,7 @@
+export default function CustomerScreen() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-200">
+      <h1 className="text-2xl font-bold">Customer Screen</h1>
+    </div>
+  );
+}
